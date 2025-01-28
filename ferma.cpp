58f18032f1137fc4c9906e1780e31ad176853f9d
natafam <93561320+natafam@users.noreply.github.com>
2025-01-28 10:10:51 +0100

    Update ferma.cpp

--- ferma.cpp
+++ ferma.cpp
@@ -1,30 +1,9 @@
-//---------------------------------------------------------------------------
-
-#include <vcl.h>
-#pragma hdrstop
-#include "Unit1.h"
-//---------------------------------------------------------------------------
-#pragma package(smart_init)
-#pragma resource "*.dfm"
-TForm1 *Form1;
-
-// Zmienne globalne dla symulacji
-const int initialChickens = 200;
-const double feedCostPerKg = 1.9;
-const double feedConsumptionPerChickenPerDay = 0.2;
-const int simulationDays = 180;
-
-//---------------------------------------------------------------------------
-__fastcall TForm1::TForm1(TComponent* Owner)
-    : TForm(Owner)
-{
-}
-
-// Funkcja do symulacji
 void SimulateFarm(int &dayReaching200, double &totalFeedCost) {
     int currentDay = 1;
     int currentChickenCount = initialChickens;
     totalFeedCost = 0.0;
+
+    bool reached200Once = false; // Flaga do śledzenia, czy już osiągnięto 200 kur
 
     dayReaching200 = -1;
 
@@ -33,9 +12,14 @@ void SimulateFarm(int &dayReaching200, double &totalFeedCost) {
         double dailyFeedCost = currentChickenCount * feedConsumptionPerChickenPerDay * feedCostPerKg;
         totalFeedCost += dailyFeedCost;
 
-        // Sprawdzenie, kiedy liczba kur ponownie osiąga 200 sztuk
-        if (currentChickenCount == 200 && dayReaching200 == -1) {
-            dayReaching200 = currentDay;
+        // Sprawdzenie, czy liczba kur osiąga 200
+        if (currentChickenCount == 200) {
+            if (!reached200Once) {
+                reached200Once = true; // Pierwsze osiągnięcie 200, ignorujemy
+            } else if (dayReaching200 == -1) {
+                dayReaching200 = currentDay; // Drugie osiągnięcie 200
+                break; // Możemy przerwać symulację, bo znaleźliśmy odpowiedź
+            }
         }
 
         // Sprawdzanie, czy to dzień nieparzysty - lis atakuje
@@ -52,24 +36,4 @@ void SimulateFarm(int &dayReaching200, double &totalFeedCost) {
         // Zwiększenie dnia
         currentDay++;
     }
-}
-
-// Obsługa przycisku "Symuluj"
-void __fastcall TForm1::ButtonSimulateClick(TObject *Sender)
-{
-    int dayReaching200;
-    double totalFeedCost;
-
-    // Uruchamiamy symulację
-    SimulateFarm(dayReaching200, totalFeedCost);
-
-    // Wyświetlamy wyniki w Memo
-    MemoResults->Lines->Clear();
-    MemoResults->Lines->Add("Wyniki symulacji:");
-    if (dayReaching200 != -1) {
-        MemoResults->Lines->Add("a) Dzień, w którym liczba kur po raz pierwszy osiąga 200 sztuk: " + IntToStr(dayReaching200));
-    } else {
-        MemoResults->Lines->Add("a) Liczba kur nigdy nie osiąga 200 sztuk w ciągu 180 dni.");
-    }
-    MemoResults->Lines->Add("b) Łączna kwota wydana na paszę: " + FloatToStrF(totalFeedCost, ffFixed, 8, 2) + " zł");
 }
